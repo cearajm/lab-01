ce55b8717fa6af45f6c3f76c1d52b16709c29c97
Ceara Mahinay <Ceara@cearas-MacBook-Air.local>
2026-01-08 21:15:13 -0700

    Add Pet class and subclasses

--- PetShop/app/src/main/java/com/example/petshop/MainActivity.java
+++ PetShop/app/src/main/java/com/example/petshop/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.SortedMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+//        Cat pet = new Pet("");
+        Cat cat = new Cat("Lucy");
+        Dog dog = new Dog("Snoopy");
+        Scorpion scorpion = new Scorpion("Scorponok");
+
+        ArrayList<Pet> petList = new ArrayList<Pet>();
+
+        petList.add(cat);
+        petList.add(dog);
+        petList.add(scorpion);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+
 }
